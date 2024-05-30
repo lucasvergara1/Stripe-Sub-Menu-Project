c4d6b099a73e2ed50cf4bedd19c2da0c5f4c0623
Lucas <lucas.vergara@live.com>
2024-05-30 00:11:18 -0300

    update

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
-import sublinks from './data.js'
-const toggleBtn = document.querySelector()
+import sublinks from "./data.js";
+
+const toggleBtn = document.querySelector(".toggle-btn");
+const closeBtn = document.querySelector(".close-btn");
+const sideWrapper = document.querySelector(".side-wrapper");
+const sidebar = document.querySelector(".sidebar-links");
+const linkBtns = [...document.querySelectorAll(".link-btn")];
+const submenu = document.querySelector(".submenu");
+const hero = document.querySelector(".hero");
+const nav = document.querySelector(".nav");
+
+//hide/show side bar
+toggleBtn.addEventListener("click", () => {
+  sideWrapper.classList.add("show");
+});
+closeBtn.addEventListener("click", () => {
+  sideWrapper.classList.remove("show");
+});
